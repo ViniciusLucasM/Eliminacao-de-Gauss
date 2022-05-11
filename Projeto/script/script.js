a6257db95d5538c5c63db590bd78d8abb07b07e1
ViniciusLucasM <viniciuslucas@edu.unifil.br>
2022-05-10 22:09:03 -0300

    Att

--- Projeto/script/script.js
+++ Projeto/script/script.js
@@ -3,9 +3,22 @@ const span = document.querySelector("#span");
 
 button.addEventListener("click", function(e) {
     e.preventDefault();
-    const element = getelement("l1");
-    const resu = element.value;
-    span.innerHTML = `<p>${resu}</p>`;
+    let valores = [];
+    for(i = 1; i < 10; i++){
+        const id = "l" + i.toString();
+        for(x = 1; x < 10; x++){
+            const element = getelement(id);
+            const resu = element.value;
+            if(valores.includes(resu) == false){
+                valores.push(resu);
+            }
+        }
+    }
+    let B = 0;
+    for(i = 0; i < 3; i++){
+        B += parseInt(valores[i]);
+    }
+    console.log(B);
 })
 
 function getelement(id){
